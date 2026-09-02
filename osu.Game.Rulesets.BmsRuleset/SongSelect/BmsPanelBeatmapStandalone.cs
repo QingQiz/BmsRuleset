32bc36d242152e06c97eec3ae5b03eb96eca3a6c
ChihayaAnon <anon@mygo.com>
2026-09-02 12:39:13 +0800

    refactor(songselect): merge beatmap panel content

--- osu.Game.Rulesets.BmsRuleset/SongSelect/BmsPanelBeatmapStandalone.cs
+++ osu.Game.Rulesets.BmsRuleset/SongSelect/BmsPanelBeatmapStandalone.cs
@@ -2,38 +2,25 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using osu.Framework.Allocation;
-using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.UserInterface;
 using osu.Framework.Localisation;
-using osu.Framework.Threading;
 using osu.Game.Beatmaps;
-using osu.Game.Rulesets.BmsRuleset.Beatmaps;
 using osu.Game.Graphics.Carousel;
 using osu.Game.Graphics.Containers;
 using osu.Game.Overlays;
 using osu.Game.Resources.Localisation.Web;
-using osu.Game.Rulesets.Mods;
 using osu.Game.Screens.Select;
 using osuTK;
 
 namespace osu.Game.Rulesets.BmsRuleset.SongSelect;
 
-internal partial class BmsPanelBeatmapStandalone : Panel
+internal partial class BmsPanelBeatmapStandalone : BmsBeatmapPanel
 {
     public const float HEIGHT = CarouselItem.DEFAULT_HEIGHT * 1.6f;
-
-    [Resolved]
-    private IBindable<RulesetInfo> ruleset { get; set; } = null!;
-
-    [Resolved]
-    private IBindable<IReadOnlyList<Mod>> mods { get; set; } = null!;
 
     [Resolved]
     private OverlayColourProvider colourProvider { get; set; } = null!;
@@ -41,24 +28,11 @@ internal partial class BmsPanelBeatmapStandalone : Panel
     [Resolved]
     private ISongSelect? songSelect { get; set; }
 
-    [Resolved]
-    private BeatmapManager beatmaps { get; set; } = null!;
-
-    [Resolved]
-    private BeatmapDifficultyCache difficultyCache { get; set; } = null!;
-
-    private IBindable<StarDifficulty>? starDifficultyBindable;
-    private CancellationTokenSource? starDifficultyCancellationSource;
-
-    private ScheduledDelegate? scheduledBackgroundRetrieval;
-    private CancellationTokenSource? backgroundCancellationSource;
-
-    private ConstrainedIconContainer difficultyIcon = null!;
-    private BmsStandaloneBeatmapContent panelContent = null!;
-
     private Box backgroundBorder = null!;
 
-    private BeatmapInfo beatmap => ((GroupedBeatmap)Item!.Model).Beatmap;
+    private BeatmapInfo beatmap => CurrentBeatmap!;
+
+    protected override bool BindSelectedToExpanded => true;
 
     public BmsPanelBeatmapStandalone()
     {
@@ -68,168 +42,49 @@ internal partial class BmsPanelBeatmapStandalone : Panel
     [BackgroundDependencyLoader]
     private void load()
     {
-        Height = HEIGHT;
-
-        Icon = difficultyIcon = new ConstrainedIconContainer
+        var difficultyIcon = new ConstrainedIconContainer
         {
             Size = new Vector2(12),
             Margin = new MarginPadding { Left = 4f, Right = 3f },
             Colour = colourProvider.Background5,
         };
 
-        Background = backgroundBorder = new Box
+        InitialisePanel(HEIGHT, true, colourProvider, difficultyIcon, backgroundBorder = new Box
         {
             RelativeSizeAxes = Axes.Both,
-        };
-
-        Content.Child = panelContent = new BmsStandaloneBeatmapContent(Selected, includeSetMetadata: true);
+        });
     }
 
-    protected override void LoadComplete()
-    {
-        base.LoadComplete();
-
-        ruleset.BindValueChanged(_ => updateKeyCount());
-        mods.BindValueChanged(_ => updateKeyCount(), true);
-
-        Selected.BindValueChanged(s =>
-        {
-            Expanded.Value = s.NewValue;
-        }, true);
-    }
-
-    protected override void PrepareForUse()
+    protected override void PrepareBeatmap()
     {
         // Carousel reuses a panel by replacing Item and calling PrepareForUse without
         // FreeAfterUse, so clear any previous background request before starting a new one.
-        scheduledBackgroundRetrieval?.Cancel();
-        scheduledBackgroundRetrieval = null;
-        backgroundCancellationSource?.Cancel();
-        backgroundCancellationSource = null;
-        panelContent.BeatmapBackground.Beatmap = null;
-
-        base.PrepareForUse();
-
+        CurrentBeatmap = ((GroupedBeatmap)Item!.Model).Beatmap;
         var beatmapSet = beatmap.BeatmapSet!;
+        ScheduleBackgroundRetrieval(beatmap);
 
-        var backgroundCancellation = backgroundCancellationSource = new CancellationTokenSource();
-        scheduledBackgroundRetrieval = Scheduler.AddDelayed(b =>
-        {
-            var working = beatmaps.GetWorkingBeatmap(b);
+        TitleText.Text = new RomanisableString(beatmapSet.Metadata.TitleUnicode, beatmapSet.Metadata.Title);
+        ArtistText.Text = new RomanisableString(beatmapSet.Metadata.ArtistUnicode, beatmapSet.Metadata.Artist);
+        UpdateButton!.BeatmapSet = beatmapSet;
+        StatusPill!.Status = beatmap.Status;
 
-            if (working is not BmsWorkingBeatmap bmsWorking)
-            {
-                panelContent.BeatmapBackground.Beatmap = working;
-                return;
-            }
+        DifficultyIcon.Icon = beatmap.Ruleset.CreateInstance().CreateIcon();
+        DifficultyIcon.Show();
 
-            bmsWorking.PrepareBackgroundMetadataAsync(backgroundCancellation.Token).ContinueWith(task => Scheduler.Add(() =>
-            {
-                if (!task.IsCompletedSuccessfully || backgroundCancellation.IsCancellationRequested || IsDisposed)
-                    return;
+        LocalRank.Beatmap = beatmap;
+        LocalRank.RefreshBmsScores();
+        DifficultyIcon.Parent!.Alpha = 0;
+        LocalRank.AttachLamp((Container)backgroundBorder.Parent!);
+        DifficultyText.Text = beatmap.DifficultyName;
+        AuthorText.Text = BeatmapsetsStrings.ShowDetailsMappedBy(beatmap.Metadata.Author.Username);
 
-                if (Item?.Model is not GroupedBeatmap grouped || !ReferenceEquals(grouped.Beatmap, b))
-                    return;
-
-                panelContent.BeatmapBackground.Beatmap = bmsWorking;
-            }), TaskScheduler.Default);
-        }, beatmap, 50);
-
-        panelContent.TitleText.Text = new RomanisableString(beatmapSet.Metadata.TitleUnicode, beatmapSet.Metadata.Title);
-        panelContent.ArtistText.Text = new RomanisableString(beatmapSet.Metadata.ArtistUnicode, beatmapSet.Metadata.Artist);
-        panelContent.UpdateButton!.BeatmapSet = beatmapSet;
-        panelContent.StatusPill!.Status = beatmap.Status;
-
-        difficultyIcon.Icon = beatmap.Ruleset.CreateInstance().CreateIcon();
-        difficultyIcon.Show();
-
-        panelContent.LocalRank.Beatmap = beatmap;
-        panelContent.LocalRank.RefreshBmsScores();
-        difficultyIcon.Parent!.Alpha = 0;
-        panelContent.LocalRank.AttachLamp((Container)backgroundBorder.Parent!);
-        panelContent.DifficultyText.Text = beatmap.DifficultyName;
-        panelContent.AuthorText.Text = BeatmapsetsStrings.ShowDetailsMappedBy(beatmap.Metadata.Author.Username);
-
-        computeStarRating();
-        panelContent.SpreadDisplay.Beatmap.Value = beatmap;
-        updateKeyCount();
+        SpreadDisplayControl.Beatmap.Value = beatmap;
     }
 
-    protected override void FreeAfterUse()
+    protected override void UpdateDifficultyColour(osuTK.Graphics.Color4 diffColour)
     {
-        base.FreeAfterUse();
-
-        scheduledBackgroundRetrieval?.Cancel();
-        scheduledBackgroundRetrieval = null;
-        backgroundCancellationSource?.Cancel();
-        backgroundCancellationSource = null;
-        panelContent.BeatmapBackground.Beatmap = null;
-        panelContent.UpdateButton!.BeatmapSet = null;
-        panelContent.LocalRank.Beatmap = null;
-        difficultyIcon.Parent!.Alpha = 1;
-        panelContent.LocalRank.DetachLamp();
-        starDifficultyBindable = null;
-        panelContent.SpreadDisplay.Beatmap.Value = null;
-
-        starDifficultyCancellationSource?.Cancel();
-    }
-
-    private void computeStarRating()
-    {
-        starDifficultyCancellationSource?.Cancel();
-        starDifficultyCancellationSource = new CancellationTokenSource();
-
-        if (Item == null)
-            return;
-
-        starDifficultyBindable = difficultyCache.GetBindableDifficulty(beatmap, starDifficultyCancellationSource.Token, BmsSongSelect.DIFFICULTY_CALCULATION_DEBOUNCE);
-        starDifficultyBindable.BindValueChanged(starDifficulty =>
-        {
-            panelContent.StarRatingDisplay.Current.Value = starDifficulty.NewValue;
-            panelContent.SpreadDisplay.StarDifficulty.Value = starDifficulty.NewValue;
-        }, true);
-    }
-
-    protected override void Update()
-    {
-        base.Update();
-
-        if (Item?.IsVisible != true)
-        {
-            starDifficultyCancellationSource?.Cancel();
-            starDifficultyCancellationSource = null;
-        }
-
-        // Dirty hack to make sure we don't take up spacing in parent fill flow when not displaying a rank.
-        // I can't find a better way to do this.
-        panelContent.MainFill.Margin = new MarginPadding { Left = 1 / panelContent.StarRatingDisplay.Scale.X * (panelContent.LocalRank.HasRank ? 0 : -3) };
-
-        var diffColour = panelContent.StarRatingDisplay.DisplayedDifficultyColour;
-
-        AccentColour = diffColour;
-        panelContent.SpreadDisplay.Current.Colour = diffColour;
-
         backgroundBorder.Colour = diffColour;
-        difficultyIcon.Colour = panelContent.StarRatingDisplay.DisplayedDifficultyTextColour;
-    }
-
-    private void updateKeyCount()
-    {
-        if (Item == null)
-            return;
-
-        var rulesetInstance = ruleset.Value.CreateInstance();
-
-        if (rulesetInstance.AvailableVariants.Count() > 1)
-        {
-            var variant = rulesetInstance.GetVariantForBeatmap(beatmap, mods.Value);
-            var variantName = rulesetInstance.GetVariantName(variant);
-
-            panelContent.KeyCountText.Alpha = 1;
-            panelContent.KeyCountText.Text = LocalisableString.Interpolate($"[{variantName}] ");
-        }
-        else
-            panelContent.KeyCountText.Alpha = 0;
+        DifficultyIcon.Colour = StarRatingDisplay.DisplayedDifficultyTextColour;
     }
 
     public override MenuItem[] ContextMenuItems
--- osu.Game.Rulesets.BmsRuleset/SongSelect/Course/BmsCourseStagePanel.cs
+++ osu.Game.Rulesets.BmsRuleset/SongSelect/Course/BmsCourseStagePanel.cs
@@ -1,9 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using osu.Framework.Allocation;
-using osu.Framework.Bindables;
+﻿using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
@@ -11,45 +6,24 @@ using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.UserInterface;
 using osu.Framework.Input.Events;
 using osu.Framework.Localisation;
-using osu.Framework.Threading;
 using osu.Game.Beatmaps;
-using osu.Game.Rulesets.BmsRuleset.Beatmaps;
 using osu.Game.Graphics.Containers;
 using osu.Game.Overlays;
 using osu.Game.Rulesets.BmsRuleset.Course;
 using osu.Game.Rulesets.BmsRuleset.Localisation;
-using osu.Game.Rulesets.Mods;
 using osu.Game.Screens.Select;
 using osuTK;
 using osuTK.Graphics;
 
 namespace osu.Game.Rulesets.BmsRuleset.SongSelect.Course;
 
-internal partial class BmsCourseStagePanel : Panel
+internal partial class BmsCourseStagePanel : BmsBeatmapPanel
 {
     private BmsCourseStage? stage;
-    private BmsStandaloneBeatmapContent panelContent = null!;
-    private ConstrainedIconContainer difficultyIcon = null!;
     private Container backgroundContainer = null!;
     private Box missingBackground = null!;
     private Color4 availableIconColour;
     private Color4 defaultAccentColour;
-    private ScheduledDelegate? scheduledBackgroundRetrieval;
-    private CancellationTokenSource? backgroundCancellationSource;
-    private IBindable<StarDifficulty>? starDifficultyBindable;
-    private CancellationTokenSource? starDifficultyCancellationSource;
-
-    [Resolved]
-    private IBindable<RulesetInfo> ruleset { get; set; } = null!;
-
-    [Resolved]
-    private IBindable<IReadOnlyList<Mod>> mods { get; set; } = null!;
-
-    [Resolved]
-    private BeatmapDifficultyCache difficultyCache { get; set; } = null!;
-
-    [Resolved]
-    private BeatmapManager beatmaps { get; set; } = null!;
 
     internal BeatmapInfo? ResolvedBeatmap { get; private set; }
 
@@ -61,11 +35,10 @@ internal partial class BmsCourseStagePanel : Panel
     [BackgroundDependencyLoader]
     private void load(OverlayColourProvider colourProvider)
     {
-        Height = PanelBeatmapStandalone.HEIGHT;
         AccentColour = defaultAccentColour = colourProvider.Highlight1;
         availableIconColour = colourProvider.Background5;
 
-        Icon = difficultyIcon = new ConstrainedIconContainer
+        var difficultyIcon = new ConstrainedIconContainer
         {
             Icon = new SpriteIcon { Icon = FontAwesome.Solid.ExclamationTriangle },
             Size = new Vector2(12),
@@ -75,7 +48,7 @@ internal partial class BmsCourseStagePanel : Panel
             AlwaysPresent = true,
         };
 
-        Background = backgroundContainer = new Container
+        backgroundContainer = new Container
         {
             RelativeSizeAxes = Axes.Both,
             Children =
@@ -88,183 +61,59 @@ internal partial class BmsCourseStagePanel : Panel
                 },
             ],
         };
-        Content.Child = panelContent = new BmsStandaloneBeatmapContent(Selected, includeSetMetadata: false);
+
+        InitialisePanel(PanelBeatmapStandalone.HEIGHT, false, colourProvider, difficultyIcon, backgroundContainer);
     }
 
-    protected override void LoadComplete()
-    {
-        base.LoadComplete();
-
-        ruleset.BindValueChanged(_ => updateKeyCount());
-        mods.BindValueChanged(_ => updateKeyCount(), true);
-    }
-
-    protected override void PrepareForUse()
+    protected override void PrepareBeatmap()
     {
         // Carousel reuse can call PrepareForUse directly when replacing the stage item.
         // Cancel the previous background request before publishing the new stage.
-        scheduledBackgroundRetrieval?.Cancel();
-        scheduledBackgroundRetrieval = null;
-        backgroundCancellationSource?.Cancel();
-        backgroundCancellationSource = null;
-        panelContent.BeatmapBackground.Beatmap = null;
-
-        resetModelState();
-        base.PrepareForUse();
-
         var model = (BmsGroupedCourseStage)Item!.Model;
         stage = model.Stage;
         ResolvedBeatmap = model.Beatmap;
+        CurrentBeatmap = ResolvedBeatmap;
         Name = $"Course stage {model.StageIndex + 1} song panel";
 
         var metadata = ResolvedBeatmap?.BeatmapSet?.Metadata ?? ResolvedBeatmap?.Metadata;
-        panelContent.TitleText.Text = metadata == null
+        TitleText.Text = metadata == null
             ? stage.Title
             : new RomanisableString(metadata.TitleUnicode, metadata.Title);
-        panelContent.ArtistText.Text = metadata == null
+        ArtistText.Text = metadata == null
             ? stage.Artist ?? string.Empty
             : new RomanisableString(metadata.ArtistUnicode, metadata.Artist);
-        panelContent.DifficultyText.Text = ResolvedBeatmap?.DifficultyName ?? (stage.IsAvailable ? stage.Difficulty : BmsStrings.CourseStageMissing);
-        panelContent.DifficultyText.Colour = stage.IsAvailable ? Color4.White : Color4.OrangeRed;
+        DifficultyText.Text = ResolvedBeatmap?.DifficultyName ?? (stage.IsAvailable ? stage.Difficulty : BmsStrings.CourseStageMissing);
+        DifficultyText.Colour = stage.IsAvailable ? Color4.White : Color4.OrangeRed;
 
-        difficultyIcon.Icon = ResolvedBeatmap?.Ruleset.CreateInstance().CreateIcon()
+        DifficultyIcon.Icon = ResolvedBeatmap?.Ruleset.CreateInstance().CreateIcon()
                               ?? new SpriteIcon { Icon = FontAwesome.Solid.ExclamationTriangle };
-        difficultyIcon.Colour = ResolvedBeatmap != null ? availableIconColour : Color4.OrangeRed;
-        difficultyIcon.Alpha = ResolvedBeatmap != null ? 0 : 1;
+        DifficultyIcon.Colour = ResolvedBeatmap != null ? availableIconColour : Color4.OrangeRed;
+        DifficultyIcon.Alpha = ResolvedBeatmap != null ? 0 : 1;
 
-        panelContent.LocalRank.Beatmap = ResolvedBeatmap;
+        LocalRank.Beatmap = ResolvedBeatmap;
         if (ResolvedBeatmap != null)
-            panelContent.LocalRank.RefreshBmsScores();
-        panelContent.LocalRank.Alpha = ResolvedBeatmap != null ? 1 : 0;
-        panelContent.LocalRank.AttachLamp(backgroundContainer);
+            LocalRank.RefreshBmsScores();
+        LocalRank.Alpha = ResolvedBeatmap != null ? 1 : 0;
+        LocalRank.AttachLamp(backgroundContainer);
         missingBackground.Alpha = ResolvedBeatmap != null ? 0 : 1;
-        panelContent.StarRatingDisplay.Alpha = ResolvedBeatmap != null ? 1 : 0;
-        panelContent.SpreadDisplay.Alpha = ResolvedBeatmap != null ? 1 : 0;
-        panelContent.SpreadDisplay.Beatmap.Value = ResolvedBeatmap;
-        panelContent.KeyCountText.Alpha = 0;
-        panelContent.StarRatingDisplay.Current.Value = default;
+        StarRatingDisplay.Alpha = ResolvedBeatmap != null ? 1 : 0;
+        SpreadDisplayControl.Alpha = ResolvedBeatmap != null ? 1 : 0;
+        SpreadDisplayControl.Beatmap.Value = ResolvedBeatmap;
 
         if (ResolvedBeatmap != null)
-        {
-            var backgroundCancellation = backgroundCancellationSource = new CancellationTokenSource();
-            scheduledBackgroundRetrieval = Scheduler.AddDelayed(b =>
-            {
-                if (!ReferenceEquals(ResolvedBeatmap, b))
-                    return;
-
-                var working = beatmaps.GetWorkingBeatmap(b);
-
-                if (working is not BmsWorkingBeatmap bmsWorking)
-                {
-                    panelContent.BeatmapBackground.Beatmap = working;
-                    return;
-                }
-
-                bmsWorking.PrepareBackgroundMetadataAsync(backgroundCancellation.Token).ContinueWith(task => Scheduler.Add(() =>
-                {
-                    if (!task.IsCompletedSuccessfully || backgroundCancellation.IsCancellationRequested || IsDisposed
-                        || !ReferenceEquals(ResolvedBeatmap, b))
-                        return;
-
-                    panelContent.BeatmapBackground.Beatmap = bmsWorking;
-                }), TaskScheduler.Default);
-            }, ResolvedBeatmap, 50);
-        }
-
-        computeStarRating();
-        updateKeyCount();
+            ScheduleBackgroundRetrieval(ResolvedBeatmap);
     }
 
-    protected override void FreeAfterUse()
+    protected override void ResetBeatmapState()
     {
-        resetModelState();
-        base.FreeAfterUse();
-    }
-
-    private void resetModelState()
-    {
-        scheduledBackgroundRetrieval?.Cancel();
-        scheduledBackgroundRetrieval = null;
-        backgroundCancellationSource?.Cancel();
-        backgroundCancellationSource = null;
-        starDifficultyCancellationSource?.Cancel();
-        starDifficultyCancellationSource = null;
-        starDifficultyBindable = null;
-        panelContent.BeatmapBackground.Beatmap = null;
-        panelContent.LocalRank.DetachLamp();
-        panelContent.LocalRank.Beatmap = null;
-        panelContent.LocalRank.Alpha = 0;
-        panelContent.SpreadDisplay.Beatmap.Value = null;
-        panelContent.SpreadDisplay.StarDifficulty.Value = default;
-        panelContent.SpreadDisplay.Current.Colour = Color4.White;
-        panelContent.StarRatingDisplay.Current.Value = default;
-        panelContent.MainFill.Margin = new MarginPadding();
-        panelContent.KeyCountText.Alpha = 0;
+        base.ResetBeatmapState();
+        missingBackground.Alpha = 0;
+        LocalRank.Alpha = 0;
+        StarRatingDisplay.Alpha = 1;
+        SpreadDisplayControl.Alpha = 1;
         AccentColour = defaultAccentColour;
         ResolvedBeatmap = null;
         stage = null;
-    }
-
-    protected override void Update()
-    {
-        base.Update();
-
-        if (stage == null || ResolvedBeatmap == null)
-            return;
-
-        AccentColour = panelContent.StarRatingDisplay.DisplayedDifficultyColour;
-        panelContent.SpreadDisplay.Current.Colour = panelContent.StarRatingDisplay.DisplayedDifficultyColour;
-        panelContent.MainFill.Margin = new MarginPadding
-        {
-            Left = 1 / panelContent.LocalRank.Scale.X * (panelContent.LocalRank.HasRank ? 0 : -3),
-        };
-    }
-
-    protected override void Dispose(bool isDisposing)
-    {
-        starDifficultyCancellationSource?.Cancel();
-        base.Dispose(isDisposing);
-    }
-
-    private void computeStarRating()
-    {
-        starDifficultyCancellationSource?.Cancel();
-
-        if (ResolvedBeatmap == null)
-            return;
-
-        var targetBeatmap = ResolvedBeatmap;
-        starDifficultyCancellationSource = new CancellationTokenSource();
-        starDifficultyBindable = difficultyCache.GetBindableDifficulty(
-            targetBeatmap,
-            starDifficultyCancellationSource.Token,
-            osu.Game.Screens.Select.SongSelect.DIFFICULTY_CALCULATION_DEBOUNCE);
-        starDifficultyBindable.BindValueChanged(difficulty =>
-        {
-            if (!ReferenceEquals(ResolvedBeatmap, targetBeatmap))
-                return;
-
-            panelContent.StarRatingDisplay.Current.Value = difficulty.NewValue;
-
-            panelContent.SpreadDisplay.StarDifficulty.Value = difficulty.NewValue;
-        }, true);
-    }
-
-    private void updateKeyCount()
-    {
-        if (ResolvedBeatmap == null || stage == null)
-            return;
-
-        var rulesetInstance = ruleset.Value.CreateInstance();
-
-        if (rulesetInstance.AvailableVariants.Count() > 1)
-        {
-            var variant = rulesetInstance.GetVariantForBeatmap(ResolvedBeatmap, mods.Value);
-            panelContent.KeyCountText.Alpha = 1;
-            panelContent.KeyCountText.Text = LocalisableString.Interpolate($"[{rulesetInstance.GetVariantName(variant)}] ");
-        }
-        else
-            panelContent.KeyCountText.Alpha = 0;
     }
 
     public override MenuItem[] ContextMenuItems => [];
